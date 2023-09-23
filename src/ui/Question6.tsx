567dc95fcabc8fcdcafb449a1b18fd96afb14998
Ayesha-chaudhry <ashijutt069@gmail.com>
2023-09-23 21:28:01 +0500

    changes

--- src/ui/Question6.tsx
+++ src/ui/Question6.tsx
@@ -31,53 +31,47 @@ const Question6 = ({ data }: any) => {
     }, [])
 
     return (
-        <Flex fontFamily={"Lemonada"} height={{sm: "100vh"}} backgroundColor={"#7EA2FF"} className='initialPosition' transitionDuration={'1000ms'} transform={'translateY(350px)'} opacity={`${style ? '0' : '1'}`}>
-            <Box height={{sm: "60%", mm: "70%", md: "70%", lg: "", xl: "60%", "2xl": "70%"}} 
-            width={{sm: "90%", mm: "80%", md: "85%", lg: "", xl: "85%", "2xl": "85%"}} 
-            margin={{sm: "auto"}} 
-            // backgroundColor={"white"}
-            display={"flex"} alignItems={"start"} gap={{sm: "10px", md: "12px", lg: "", xl:"25px", "2xl": "25px"}}>
+        <Flex height={{ sm: "100vh" }} backgroundColor={"#7EA2FF"} className='initialPosition' transitionDuration={'1000ms'} transform={'translateY(350px)'} opacity={`${style ? '0' : '1'}`}>
+      <Box height={{ sm: "60%", mm: "70%", md: "70%", lg: "60%", xl: "60%", "2xl": "70%" }}
+        width={{ sm: "90%", md: "85%", lg: "", xl: "85%", "2xl": "85%" }}
+        margin={{ sm: "auto" }}
+        display={"flex"} alignItems={"start"} 
+        gap={{ sm: "12px", md: "20px", lg: "20px", xl: "30px", "2xl": "25px" }}>
 
-                <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"} >
-                    <Text fontSize={{ sm: "14px",
-            md: "18px",
-            lg: "24px",
-            "2xl": "48px" }} textColor={"#FFF"}
-                    >6</Text>
-                    <Text fontSize={{ sm: "14px",
-            md: "18px",
-            lg: "24px",
-            "2xl": "48px" }} textColor={"#FFF"}>
-                        <BiRightArrowAlt />
-                    </Text>
-                </Box>
+        <Box display={"flex"} justifyContent={"space-around"} alignItems={"center"} >
+          <Text fontSize={{ sm: "16px", ml: "16px", md: "22px", lg: "30px", xl: "40px", "2xl": "48px" }}
+            textColor={"#FFF"}>6</Text>
+          <Text fontSize={{ sm: "12px", ml: "12px", md: "14px", lg: "20px", xl: "28px", "2xl": "35px" }}
+            textColor={"#FFF"}>
+            <BiRightArrowAlt />
+          </Text>
+        </Box>
 
-                <Box >
-                    <Box>
-                        <Text fontSize={{sm: "14px",
-            md: "18px",
-            lg: "24px",
-            "2xl": "48px"  }} 
-                        textColor={"#FFF"}
-                        
-                        paddingX={{sm: "1px", md: "2px", lg:"0px", xl:"15px"}}
-                        >What’s your monthly household income?</Text>
-                        <Text fontSize={{ sm: "10px", mm: "12px", md: "16px", lg: "", xl: "24px", "2xl": "32px" }} 
-                        textColor={"#A5BFFF"}
-                            paddingTop={{ sm: "4px", md: "10px", lg: "", xl: "12px", "2xl": "15px" }}
-                            pl={{ sm: "0px", md: "2px", lg: "3px", xl: "18px", "2xl": "0px" }} >We won’t tell the Government so don’t worry!</Text>
-                    </Box>
-                    <Box display={{sm:"flex"}}
-                        gap={{ sm: "10px", md: "", lg: "", xl: "30px", "2xl": "30px" }} 
-                        paddingY={{ sm: "15px", mm: "20px", md: "30px", lg: "", xl: "34px", "2xl": "64px" }}
-                        pl={{ sm: "0px", md: "0px", lg: "3px", xl: "18px", "2xl": "0px" }}>
-                            
-                        <Question/>
-                    </Box>
-                </Box>
+        <Box display={"flex"} flexDir={"column"} >
+          <Box>
+            <Text fontSize={{ sm: "16px", ml: "16px", md: "22px", lg: "30px", xl: "40px", "2xl": "48px" }} 
+            textColor={"#FFF"} > What’s your monthly household income?</Text>
+            <Text fontSize={{ sm: "10px", ml: "10px", md: "14px", lg: "20px", xl: "28px", "2xl": "32px" }}
+              textColor={"#A5BFFF"}
+              paddingTop={{ sm: "4px", md: "10px", lg: "", xl: "15px", "2xl": "15px" }}
+            >We won’t tell the Government so don’t worry!</Text>
+          </Box>
+          <Box>
+            <Box display={{ sm: "flex" }}
+              gap={{ sm: "10px", md: "", lg: "", xl: "30px", "2xl": "30px" }}
+              paddingY={{ sm: "15px", mm: "20px", md: "30px", lg: "30px", xl: "34px", "2xl": "64px" }}>
+              <Question />
             </Box>
+          </Box>
+        </Box>
 
-        </Flex>
+
+        <Image src="/images//cash.gif" w={{"2xl": "350px", md: "180px", ml: "100px", sm: "80px" }} mt={{"2xl": "-174px", lg: "-92px", sm:"-24px", mm:"-32px", md:"-94px"}} ml={{"2xl": "94px", lg:"42px", md:"-12px", mm:"-22px", sm:"-24px"}} />
+
+
+      </Box>
+
+    </Flex>
     )
 }
 
